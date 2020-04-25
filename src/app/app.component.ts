53a5e78bc9ac97cd3cc485de91e365e897739b23
WefNET <john.wefler@gmail.com>
2020-04-25 10:47:16 -0500

    Dope stuff, big brain needed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WPService } from 'src/services/wp.service';
+
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mello-app';
+  posts; categories; tags;
+
+  constructor(private _wp: WPService) { }
+  
+  ngOnInit() {
+      let postsData = this._wp.getPosts();
+      let categoryData = this._wp.getCategories();
+      let tagsData = this._wp.getTags();
+  
+      forkJoin([postsData, categoryData, tagsData]).subscribe(results => {
+        this.posts = results[0];
+        this.categories = results[1];
+        this.tags = results[2];
+
+        console.log(results);
+      });
+  }
 }
